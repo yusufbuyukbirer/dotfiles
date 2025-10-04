9baaeb7561accb96b3c546c0c40a97561622a30c
yusufbuyukbirer <yusufbuyukbirer@gmail.com>
2025-10-04 15:28:27 +0300

    feat: adding updates

--- run.sh
+++ run.sh
@@ -1,82 +1,32 @@
-#!usr/bin/env bash
+#!/usr/bin/env bash
 
-aur_packages=(
-  anytype-bin
-  betterbird-bin
-  bottles
-  brave-bin
-  czkawka-git
-  ente-auth-bin
-  extension-manager
-  flowtime
-  fluent-reader
-  frogdata
-  keypunch-git
-  localsend-bin
-  losslesscut-bin
-  modrinth-app-bin
-  mousam
-  onlyoffice-bin
-  peazip
-  protonup-qt-bin
-  pycharm-community-edition-bin
-  ruffle-nightly-bin
-  rustdesk
-  rustdesk-debug
-  sublime-text-4
-  ticketbooth
-  tuxedo-control-center-bin
-  tuxedo-drivers-dkms
-  tuxguitar
-  upscayl-bin
-  video-downloader
-  wlogout
-  xnconvert
-  zen-browser-bin
-)
+set -euo pipefail
+IFS=$'\n\t'
 
-official_packages=(
-  ananicy-cpp
-  blanket
-  bleachbit
-  btop
-  celluloid
-  collision
-  dconf-editor
-  discord
-  easyeffects
-  fastfetch
-  fretboard
-  ghostty
-  handbrake
-  komikku
-  linux-zen
-  lsd
-  lutris
-  metadata-cleaner
-  mousai
-  neovim
-  nvidia-dkms
-  nvidia-prime
-  obs-studio
-  ocrfeeder
-  okular
-  proton-vpn-gtk-app
-  steam
-  syncthing
-  timeshift
-  tldr
-  valuta
-  virt-manager
-  yazi
-)
+source "packages.conf"
 
+# -- AUR Paketlerini yükler --
 echo "AUR Paketleri Yükleniyor..."
-for aur_package in ${aur_packages[@]}; do
+for aur_package in "${aur_packages[@]}"; do
   yay -S --noconfirm "$aur_package"
 done
 
+# -- Resmi Paketleri yükler
 echo "Resmi Depolar Yükleniyor..."
-for official__package in ${official_packages[@]}; do
-  sudo pacman -S --noconfirm "$aur_package"
+for official_package in "${official_packages[@]}"; do
+  sudo pacman -S --needed --noconfirm "$official_package"
+done
+
+# -- pipx yolunu bulur --
+export PATH="$HOME/.local/bin:$PATH"
+
+# -- Uzantıları yükler --
+for ext in "${extensions[@]}"; do
+    if ! gext list | grep -Fxq "$ext"; then
+        echo "Extension yükleniyor: $ext"
+        gext install "$ext"
+        echo "$ext kuruldu."
+    else
+        echo "Extension zaten kurulu: $ext"
+    fi
 done
